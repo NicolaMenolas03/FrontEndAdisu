b0543553f91d89d63fa548f498c0b87e0d42a286
mehja007 <gabrimehja007@gmail.com>
2024-12-27 19:27:05 +0100

    To-do: Responsive circles in landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,50 +1,55 @@
-import { Image, Text, View } from "react-native";
-import { blue } from "react-native-reanimated/lib/typescript/Colors";
+import React from 'react';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
-export default function Index() {
+const { width, height } = Dimensions.get('window');
+
+export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-      }}
-    >
-     
-      {/* Immagine */}
+    <View style={{ 
+      flex: 1, 
+      backgroundColor: "#FFFFFF",
+      }}>
       <Image
         source={require("@/assets/images/LogoAdisu.jpg")}
-        style={{
-          flex: 1,
-          resizeMode: "contain", // Facoltativo, a seconda di come vuoi visualizzare l'immagine
-          alignSelf: "center",
-        }}
+        style={LogoAdisu.headerImage}
       />
-          {/* Cerchio in alto a sinistra */}
-
-      <View 
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          backgroundColor: "#007FFF",
-          width: "14rem",
-          height: "14rem",
-          borderBottomRightRadius: "14rem", // metà del width/height per fare un cerchio
-        }}
-      ></View>
-
+      {/* Cerchio in alto a sinistra */}
+      <View style={Circle.Top}></View>
       {/* Cerchio in basso a destra */}
-      <View
-        style={{
-          position: "absolute",
-          bottom: 0,
-          right: 0,
-          backgroundColor: "#007FFF",
-          width: "14rem",
-          height: "14rem",
-          borderTopLeftRadius: "14rem", // metà del width/height per fare un cerchio
-        }}
-      ></View>
+      <View style={Circle.Bottom}></View>
     </View>
   );
 }
+
+const LogoAdisu = StyleSheet.create({
+  headerImage: {
+    alignSelf: 'center',
+    flex: 1,
+    resizeMode: "contain",
+    width: width * 0.5, // 50% of screen width
+    height: height * 0.2, // 20% of screen height
+  },
+});
+
+const Circle = StyleSheet.create({
+  Top: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    backgroundColor: "#007FFF",
+    width: width * 0.50, // 40% of screen width
+    height: height * 0.25, // 40% of screen width to make it a circle
+    borderBottomRightRadius: 1000 // half of the width to make a circle
+  },
+  Bottom: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#007FFF",
+    width: width * 0.50, // 40% of screen width
+    height: height * 0.25, // 40% of screen width to make it a circle
+    borderTopLeftRadius: 1000, // half of the width to make a circle
+  },
+});
+
+
