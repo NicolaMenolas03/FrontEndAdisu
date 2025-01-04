ba100bfeed6482ce9df09d2b72f66611e17f4768
Nicola Menolascina <nicola.menola1505@gmail.com>
2025-01-04 19:15:45 +0100

    Added backend on SimulationBorsaDiStudio

--- app/BorsaDiStudio/SimulazioneBorsaDiStudio.tsx
+++ app/BorsaDiStudio/SimulazioneBorsaDiStudio.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Navbar from '@/components/Navbar';
+import { apiService } from '@/services/api';
+import { useCRUD } from '@/hooks/useCRUD';
+
 
 type RootStackParamList = {
   Home: undefined;
@@ -18,15 +21,71 @@ type SimulationResults = {
   importoTotale: string;
 };
 
+interface IseeData {
+  id: number;
+  nrRange: number;
+  iseeMin: string;
+  iseeMax: string;
+  academicYear: string;
+}
+
+interface AcademicYear {
+  id: number;
+  academicYear: string;
+}
+
+
 export default function SimulazioneBorsaDiStudio() {
+  const { data: IseeData, loading: IseeLoading, error: IseeError, createItem: createIseeItem, updateItem: updateIseeItem, deleteItem: deleteIseeItem }  = useCRUD<IseeData>('/isee-range/');
+  const { data: academicYearData, loading: academicYearLoading, error: academicYearError, createItem: createAcademicYearItem, updateItem: updateAcademicYearItem, deleteItem: deleteAcademicYearItem } = useCRUD<AcademicYear>('/academicyear/');
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const [showResults, setShowResults] = useState(false);
+  const [showResults, setShowResults] = useState(false); 
   const [results, setResults] = useState<SimulationResults | null>(null);
-  const [isee, setIsee] = useState('');
   const [tipologiaStudente, setTipologiaStudente] = useState('');
+  const [anniAccademici, setAnniAccademici] = useState<any[]>([]);
+  const [selectedAnno, setSelectedAnno] = useState<string>('');
+  const [isee, setIsee] = useState<string>('');  // Valore ISEE
+  const [isees, setIsees] = useState<any[]>([]); // Array dei range ISEE
+  const [selectedRange, setSelectedRange] = useState<string>(''); // Valore del range selezionato
   const [disabilita, setDisabilita] = useState(false);
   const [pastiAggiuntivi, setPastiAggiuntivi] = useState(false);
   const [corsoSTEM, setCorsoSTEM] = useState(false);
+  
+  
+  useEffect(() => {
+    const fetchFirstYear = async () => {
+      try {
+        const response = await apiService.get(`/academicyear/`);
+        const { data } = response;
+        console.log(data);
+  
+        // Supponendo che data sia un array di anni accademici
+        if (data && data.length > 0) {
+          handleAnnoAccademico(data[0].academicYear); // Usa il primo anno accademico
+        }
+      } catch (error) {
+        console.error("Errore nel recuperare gli anni accademici:", error);
+      }
+    };
+
+    const fetchAnniAccademici = async () => {
+      try {
+        const response = await apiService.get('/academicyear/');
+        const { data } = response;
+  
+        // Assumi che `data` sia un array di anni accademici
+        if (data && data.length > 0) {
+          setAnniAccademici(data);
+          setSelectedAnno(data[0].academicYear); // Imposta il primo anno come selezionato
+        }
+      } catch (error) {
+        console.error("Errore nel recuperare gli anni accademici:", error);
+      }
+    };
+  
+    fetchAnniAccademici();
+    fetchFirstYear(); // Chiama la funzione
+  }, []); // L'array vuoto garantisce l'esecuzione al montaggio
 
   const handleSimulaPress = () => {
     const calcoloSimulazione = () => {
@@ -64,11 +123,14 @@ export default function SimulazioneBorsaDiStudio() {
     setShowResults(true);
   };
 
-
-   const handleIseeChange = (text: string) => {
-    // Rimuovi tutti i caratteri non numerici
-    const numericText = text.replace(/[^0-9]/g, '');
-    setIsee(numericText);
+  const handleAnnoAccademico = async (itemValue: string) => {    
+    console.log("Anno selezionato:", itemValue);
+    setSelectedAnno(itemValue);
+    let response = await apiService.get(`/iseerange/get-isee-range/?academicYear=${itemValue}`);	
+    const { data } = response;
+    if (response.status == 200) {
+      setIsees(data);  // Imposta i range ISEE ricevuti
+    }
   };
 
   return (
@@ -81,28 +143,41 @@ export default function SimulazioneBorsaDiStudio() {
             <Text style={styles.inputLabel}>Dati personali</Text>
             
             <Text>Anno accademico</Text>
+            {anniAccademici.length > 0 ? (
             <Picker
-              selectedValue={tipologiaStudente}
+              selectedValue={selectedAnno}
               style={styles.picker}
-              onValueChange={(itemValue: string) => setTipologiaStudente(itemValue)}
+              onValueChange={(itemValue: string) => handleAnnoAccademico(itemValue)}
             >
-              <Picker.Item label="Matricola" value="Matricola" />
-              <Picker.Item label="2 anno" value="2 anno"/>
-              <Picker.Item label="3 anno" value="3 anno"/>
-              <Picker.Item label="4 anno laurea ciclo unico" value="4 anno laurea ciclo unico"/>
-              <Picker.Item label="5 anno laurea ciclo unico" value="5 anno laurea ciclo unico"/>
-              <Picker.Item label="1 anno Fuoricorso" value="1 anno Fuoricorso"/>
-              <Picker.Item label="2 anno Fuoricorso" value="2 anno Fuoricorso"/>
+              {anniAccademici.map((annoAccademico) => (
+                <Picker.Item
+                  label={`${annoAccademico.academicYear}` }
+                  value={annoAccademico.academicYear}
+                />
+              ))}
             </Picker>
+          ) : (
+            <Text>Seleziona un anno accademico per caricare i range ISEE.</Text>
+          )}
 
             <Text>Valore ISEE</Text>
-            <TextInput
-              style={styles.input}
-              value={isee}
-              onChangeText={handleIseeChange}
-              placeholder="Inserisci il valore ISEE"
-              keyboardType="numeric"
-            />
+            {isees.length > 0 ? (
+            <Picker
+              selectedValue={selectedRange}
+              style={styles.picker}
+              onValueChange={(itemValue: string) => setSelectedRange(itemValue)}
+            >
+              {isees.map((range) => (
+                <Picker.Item
+                  key={range.nrRange}
+                  label={`Range ISEE ${range.nrRange}: ${range.iseeMin} - ${range.iseeMax}`}
+                  value={range.nrRange}
+                />
+              ))}
+            </Picker>
+          ) : (
+            <Text>Seleziona un anno accademico per caricare i range ISEE.</Text>
+          )}
 
             <Text>Tipologia Studente</Text>
             <Picker
