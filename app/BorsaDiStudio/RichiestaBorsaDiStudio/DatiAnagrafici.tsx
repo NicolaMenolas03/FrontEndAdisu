6922b642ac0a31389efb0fbbe51da0badd50831c
Nicola Menolascina <nicola.menola1505@gmail.com>
2025-02-04 19:41:00 +0100

    Change position of date of birth

--- app/BorsaDiStudio/RichiestaBorsaDiStudio/DatiAnagrafici.tsx
+++ app/BorsaDiStudio/RichiestaBorsaDiStudio/DatiAnagrafici.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
-import { TextInput, Button, Card, HelperText,Switch } from 'react-native-paper';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { TextInput, Button, Card, HelperText, Switch } from 'react-native-paper';
 import { Picker } from '@react-native-picker/picker';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -74,106 +74,106 @@ export default function DatiAnagraficiPage() {
 
   return (
     <View style={styles.container}>
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.topbar}>
-      <HomePage />
-      <Text style={styles.title}>Dati Anagrafici</Text>
-      </View>
-      
-      <Card style={styles.card}>
-        <Card.Content>
-        <Text style={styles.label}>Intestazione</Text>
-          <TextInput
-            label="Nome"
-            value={formDatiAnagrafici.nome}
-            onChangeText={(text) => handleInputChange('nome', text)}
-            mode="outlined"
-            style={styles.input}
-            theme={{ colors: { primary: '#007BFF' } }}
-          />
-          {errors.nome ? <HelperText type="error">{errors.nome}</HelperText> : null}
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.topbar}>
+          <HomePage />
+          <Text style={styles.title}>Dati Anagrafici</Text>
+        </View>
 
-          <TextInput
-            label="Cognome"
-            value={formDatiAnagrafici.cognome}
-            onChangeText={(text) => handleInputChange('cognome', text)}
-            mode="outlined"
-            style={styles.input}
-            theme={{ colors: { primary: '#007BFF' } }}
-          />
-          {errors.cognome ? <HelperText type="error">{errors.cognome}</HelperText> : null}
+        <Card style={styles.card}>
+          <Card.Content>
+            <Text style={styles.label}>Intestazione</Text>
+            <TextInput
+              label="Nome"
+              value={formDatiAnagrafici.nome}
+              onChangeText={(text) => handleInputChange('nome', text)}
+              mode="outlined"
+              style={styles.input}
+              theme={{ colors: { primary: '#007BFF' } }}
+            />
+            {errors.nome ? <HelperText type="error">{errors.nome}</HelperText> : null}
 
-          <Text style={styles.label}>Sesso</Text>
-          <Picker
-            selectedValue={formDatiAnagrafici.sesso}
-            onValueChange={(itemValue) => handleInputChange('sesso', itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleziona Sesso" value="" />
-            <Picker.Item label="Maschio" value="Maschio" />
-            <Picker.Item label="Femmina" value="Femmina" />
-            <Picker.Item label="Altro" value="Altro" />
-          </Picker>
-          {errors.sesso ? <HelperText type="error">{errors.sesso}</HelperText> : null}
+            <TextInput
+              label="Cognome"
+              value={formDatiAnagrafici.cognome}
+              onChangeText={(text) => handleInputChange('cognome', text)}
+              mode="outlined"
+              style={styles.input}
+              theme={{ colors: { primary: '#007BFF' } }}
+            />
+            {errors.cognome ? <HelperText type="error">{errors.cognome}</HelperText> : null}
 
-          <TextInput
-            label="Data di Nascita (GG/MM/AAAA)"
-            value={formDatiAnagrafici.etaNascita}
-            onChangeText={(text) => handleInputChange('etaNascita', text)}
-            mode="outlined"
-            keyboardType="numeric"
-            style={styles.input}
-            theme={{ colors: { primary: '#007BFF' } }}
-          />
-          {errors.etaNascita ? <HelperText type="error">{errors.etaNascita}</HelperText> : null}
+            <TextInput
+              label="Data di Nascita (GG/MM/AAAA)"
+              value={formDatiAnagrafici.etaNascita}
+              onChangeText={(text) => handleInputChange('etaNascita', text)}
+              mode="outlined"
+              keyboardType="numeric"
+              style={styles.input}
+              theme={{ colors: { primary: '#007BFF' } }}
+            />
+            {errors.etaNascita ? <HelperText type="error">{errors.etaNascita}</HelperText> : null}
 
-          <Text style={styles.label}>Cittadinanza</Text>
-          <Picker
-            selectedValue={formDatiAnagrafici.cittadinanza}
-            onValueChange={(itemValue) => handleInputChange('cittadinanza', itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleziona Cittadinanza" value="" />
-            <Picker.Item label="Italiana" value="Italiana" />
-            <Picker.Item label="Statunitense" value="Statunitense" />
-            <Picker.Item label="Francese" value="Francese" />
-            <Picker.Item label="Tedesca" value="Tedesca" />
-            <Picker.Item label="Spagnola" value="Spagnola" />
-            <Picker.Item label="Portoghese" value="Portoghese" />
-            <Picker.Item label="Inglese" value="Inglese" />
-            <Picker.Item label="Cinese" value="Cinese" />
-            <Picker.Item label="Giapponese" value="Giapponese" />
-            <Picker.Item label="Indiana" value="Indiana" />
-            <Picker.Item label="Altro" value="Altro" />
-          </Picker>
-          {errors.cittadinanza ? <HelperText type="error">{errors.cittadinanza}</HelperText> : null}
+            <Text style={styles.label}>Sesso</Text>
+            <Picker
+              selectedValue={formDatiAnagrafici.sesso}
+              onValueChange={(itemValue) => handleInputChange('sesso', itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleziona Sesso" value="" />
+              <Picker.Item label="Maschio" value="Maschio" />
+              <Picker.Item label="Femmina" value="Femmina" />
+              <Picker.Item label="Altro" value="Altro" />
+            </Picker>
+            {errors.sesso ? <HelperText type="error">{errors.sesso}</HelperText> : null}
+
+            <Text style={styles.label}>Cittadinanza</Text>
+            <Picker
+              selectedValue={formDatiAnagrafici.cittadinanza}
+              onValueChange={(itemValue) => handleInputChange('cittadinanza', itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleziona Cittadinanza" value="" />
+              <Picker.Item label="Italiana" value="Italiana" />
+              <Picker.Item label="Statunitense" value="Statunitense" />
+              <Picker.Item label="Francese" value="Francese" />
+              <Picker.Item label="Tedesca" value="Tedesca" />
+              <Picker.Item label="Spagnola" value="Spagnola" />
+              <Picker.Item label="Portoghese" value="Portoghese" />
+              <Picker.Item label="Inglese" value="Inglese" />
+              <Picker.Item label="Cinese" value="Cinese" />
+              <Picker.Item label="Giapponese" value="Giapponese" />
+              <Picker.Item label="Indiana" value="Indiana" />
+              <Picker.Item label="Altro" value="Altro" />
+            </Picker>
+            {errors.cittadinanza ? <HelperText type="error">{errors.cittadinanza}</HelperText> : null}
 
             <View style={styles.switchContainer}>
-                <Text style={styles.label}>Disabilità</Text>
-                <Switch
+              <Text style={styles.label}>Disabilità</Text>
+              <Switch
                 value={formDatiAnagrafici.disabilita}
                 onValueChange={(value) => handleInputChange('disabilita', value)}
                 color='#007BFF'
               />
             </View>
-          
-        </Card.Content>
-      </Card>
-      <View style={styles.buttonContainer}>
-                  <TouchableOpacity style={styles.box} onPress={handleNext}>
-                    <Text style={styles.buttonText}>Successivo</Text>
-                  </TouchableOpacity>
-                </View>
-      
-    </ScrollView>
-    <GufoChat></GufoChat>
+
+          </Card.Content>
+        </Card>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.box} onPress={handleNext}>
+            <Text style={styles.buttonText}>Successivo</Text>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView>
+      <GufoChat></GufoChat>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     justifyContent: 'space-between',
     backgroundColor: '#f5f5f5',
   },
@@ -237,7 +237,7 @@ const styles = StyleSheet.create({
   buttonTextStyle: {
     fontSize: 16,
     marginBottom: 5,
-    color:'white',
+    color: 'white',
     fontWeight: 'bold',
   },
   buttonContainer: {
@@ -247,11 +247,11 @@ const styles = StyleSheet.create({
   },
   box: {
     backgroundColor: '#007FFF',
-        padding: 10,
-        borderRadius: 10,
-        width: 300,
-        alignItems: 'center',
-        marginTop: 10,
+    padding: 10,
+    borderRadius: 10,
+    width: 300,
+    alignItems: 'center',
+    marginTop: 10,
   },
   topbar: {
     flexDirection: 'row',
@@ -260,11 +260,11 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   buttonText: {
-    marginLeft:15,
-    marginRight:15,
+    marginLeft: 15,
+    marginRight: 15,
     fontSize: 16,
     fontWeight: '500',
     marginBottom: 5,
-    color:'white',
+    color: 'white',
   },
 });
