c52fb03813e630d18582f53986a0c9abee5aef84
DomenicoVernone <d.vernone200217@libero.it>
2025-02-17 23:33:32 +0100

    Added message for Password not correct, and added modal from for registration complete

--- app/(user)/Registration.tsx
+++ app/(user)/Registration.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, StyleSheet, Dimensions, ScrollView, TouchableOpacity, } from "react-native";
-import { TextInput, Text, Title } from "react-native-paper";
+import { View, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Modal, } from "react-native";
+import { TextInput, Text, Title, HelperText } from "react-native-paper";
 import LogoAdisu from "@/components/LogoAdisu";
 import { authService } from "@/services/api";
 import { useRouter } from "expo-router";
@@ -13,17 +13,33 @@ export default function Registration() {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [password2, setPassword2] = React.useState("");
+    const [errors, setErrors] = React.useState({ password: "" });
     const router = useRouter();
+    const [isModalVisible, setModalVisible] = React.useState(false);
+
+    const handleHomePress = () => {
+        setModalVisible(true);
+    };
+
+    const confirmExit = async () => {
+        setModalVisible(false);
+        router.push("./Login");
+    };
 
     const register = async () => {
         let response = await authService.register({
             first_name: name, last_name: surname, username: username, email: email, password: password, password2: password2,
         });
-        if (response.status == 201) {
-            router.push("../(tabs)/LandingPage");
+
+        if (response.status === 201) {
+            handleHomePress();
         } else {
-            // TODO: Create un messaggio di errore
-            alert(response);
+            if (password !== password2) {
+                setErrors({ password: "Le password non corrispondono" });
+            }
+            else if (response.status === 400) {
+                setErrors({ password: "La password inserita non rispetta i criteri di sicurezza richiesti. Assicurati che la tua password contenga una combinazione di lettere maiuscole, lettere minuscole, numeri e simboli speciali, e che sia di lunghezza adeguata." });
+            }
         }
     };
 
@@ -72,6 +88,9 @@ export default function Registration() {
                     onChangeText={(text) => setPassword(text)}
                     theme={{ colors: { primary: "#007BFF" } }}
                 />
+                {errors.password ? (
+                    <HelperText style={styles.errorText} type="error">{errors.password}</HelperText>
+                ) : null}
                 <TextInput
                     label="Confirm Password"
                     mode="outlined"
@@ -90,6 +109,24 @@ export default function Registration() {
                     </Text>
                 </TouchableOpacity>
             </View>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={isModalVisible}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalText}>
+                            Registrazione avvenuta con successo!
+                        </Text>
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity style={styles.boxTextModalButton} onPress={confirmExit}>
+                                <Text style={styles.buttonTextModal}>Accedi</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </ScrollView>
     );
 }
@@ -140,5 +177,52 @@ const styles = StyleSheet.create({
         fontSize: 13,
         backgroundColor: "#FFFFFF",
         marginBottom: 100,
+    },
+    errorText: {
+        color: "red",
+        margin: 10,
+    },
+
+    //Finestra modale 
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 5,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    modalContent: {
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        width: "80%",
+        alignItems: "center",
+    },
+    modalText: {
+        color: "green",
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 20,
+        textAlign: "center",
+    },
+    boxTextModalButton: {
+        backgroundColor: "#007FFF",
+        padding: 10,
+        borderRadius: 10,
+        width: 100,
+        alignItems: "center",
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    buttonTextModal: {
+        marginLeft: 15,
+        marginRight: 15,
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "white",
     },
 });
